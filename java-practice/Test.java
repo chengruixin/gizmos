a4ffb917679b706925b9e28b9063533402973194
chengruixin <chengruixin_2010@163.com>
2020-12-13 20:57:28 +0800

    add changes

--- java-practice/Test.java
+++ java-practice/Test.java
@@ -1,13 +1,7 @@
-import mp.MyMath;
-import mp.Person;
-import java.util.Date;
 
 class Test {
     public static void main(String [] args) {
-        Date bir = new Date();
 
-        System.out.println(bir.toString());
-        System.out.println(new Date());
     }
 
     
