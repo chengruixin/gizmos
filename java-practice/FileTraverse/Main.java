5c68e4b5521a9eab9ad869d0c8bd4b804f8d7fb4
chengruixin <chengruixin_2010@163.com>
2020-12-20 23:15:15 +0800

    complete file traverser

--- java-practice/FileTraverse/Main.java
+++ java-practice/FileTraverse/Main.java
@@ -1,90 +1,123 @@
 package FileTraverse;
 
 import java.io.File;
-// import java.io.FileFilter;
 import java.util.ArrayList;
-import java.util.Hashtable;
-// import java.util.Hashtable;
 import java.io.FileWriter;
 import java.io.BufferedWriter;
 
 
 public class Main {
-    public static ArrayList<File> fileCollections = new ArrayList<File>();
-    public static ArrayList<FileInfo> informations = new ArrayList<FileInfo>();
-    public static Hashtable<String, Integer> hashMap = new Hashtable<String, Integer>();  
+    
     public static void main(String[] args) {
-        String search = "avsa";
+        // String[] searchingDirs = {
+        //     "D:\\intermediate",
+        //     "D:\\movies",
+        //     "E:\\Theatre\\wifes",
+        //     "F:\\Downloads",
+        //     "G:\\迅雷下载",
+        //     "G:\\MoviesFromDrive"
+        // };
+        // String[] searchQueries = {"135", "133"};
+        
         String[] searchingDirs = {
-            "D:\\intermediate",
-            "D:\\movies",
-            "E:\\Theatre\\wifes",
-            "F:\\Downloads",
-            "G:\\迅雷下载",
-            "G:\\MoviesFromDrive"
+            "E:\\Theatre\\wifes"
         };
+        String[] searchQueries = {"as"};
         
-        for(String entryPoint : searchingDirs){
-            File file = new File(entryPoint);
-            exploreFiles(file);
-            // break;
-        }
+        FileWorker fileWer = new FileWorker();
+        fileWer.writeFoundFileInfo(searchingDirs, searchQueries, "./all.txt");
         
-        // String[] suffix = new String[fileCollections.size()];
-
-        for(int i = 0; i<fileCollections.size(); i++){
-            String name = fileCollections.get(i).getName().toLowerCase();
-            String pathUrl = fileCollections.get(i).getPath();
-            if(getFileNameNoEx(name).contains(search)){
-                informations.add(new FileInfo(name, pathUrl));
-            }
-            // if(hashMap.get(name) == null){
-            //     hashMap.put(name, 1);
-            // }
-            // else{
-            //     hashMap.put(name, hashMap.get(name) + 1);
-            //     System.out.println("Replicated " +  pathUrl + " " + name);
-            // }
-           
-        }
-
-        /**
-         * Write info into txt file
-         */
-        try{
-            File file = new File("Output.txt");
-            if(!file.exists()){
-                file.createNewFile();
-            }
-            FileWriter fileWriter = new FileWriter(file.getName(),true);
-            BufferedWriter myWriter = new BufferedWriter(fileWriter);
-            myWriter.flush();
-            // myWriter.write("Total " + "'" + search + "'" + "found : " + informations.size());
-            for(FileInfo info : informations){
-                myWriter.write(info.getFileName() + "\n" + info.getFilePath() + "\n\n");
-            }
-            myWriter.close();
-            System.out.println("done");
-        }
-        catch(Exception e){
-            System.out.println(e);
-        }
 
     }
     
-    public static void exploreFiles(File file){
+    
+}
+
+class FileInfo {
+    private String name;
+    private String path;
+    private long size;
+
+    FileInfo(File file){
+        name = file.getName();
+        path = file.getPath();
+        size = file.length();
+    }
+
+    FileInfo(String n, String p){
+        name = n;
+        path = p;
+    }
+
+    FileInfo(String n, String p, long s){
+        name = n;
+        path = p;
+        size = s;
+    }
+    public String getFileName(){
+        return name;
+    }
+
+    public String getFilePath(){
+        return path;
+    }
+
+    public double getSizeInGigabytes(){
+        // return size;
+        double raw = (double) size / 1024 / 1024 / 1024;
+        return Double.parseDouble(String.format("%.2f", raw));
+        // double c=Double.parseDouble(String.format("%.2f", a));
+    }
+
+    public String concreteInfo(){
+        return getFileName() + " SIZE: " + getSizeInGigabytes() + "G" + "\n" + getFilePath() + "\n\n";
+    }
+}
+
+class FileExplorer {
+
+    public ArrayList<File> getAllFiles(String[] dirs){
+        ArrayList<File> allFiles = new ArrayList<File>();
+        
+        for(String dir : dirs){
+            File file = new File(dir);
+            exploreAndAddFiles(file, allFiles);
+        }
+
+        return allFiles;
+    }
+    public ArrayList<FileInfo> getMatchedFileInfo(ArrayList<File> readFrom, String[] searchQuery){
+        ArrayList<FileInfo> ans = new ArrayList<FileInfo>();
+
+        for(int i = 0; i<readFrom.size(); i++){
+            File f = readFrom.get(i);
+            String name = f.getName().toLowerCase();
+            
+            for(int j = 0; j < searchQuery.length; j++){
+                if(getFileNameNoEx(name).contains(searchQuery[j])){
+                    ans.add(new FileInfo(f));
+                    break;
+                }
+            }
+           
+        }
+
+        return ans;
+    }
+    public void exploreAndAddFiles(File file, ArrayList<File> ans){
         File[] files = file.listFiles();
+        if(files == null) return;
         for(File f : files){
             if(f.isDirectory()){
-                exploreFiles(f);
+                exploreAndAddFiles(f, ans);
             }
             if(f.isFile()){
-                fileCollections.add(f);
+                ans.add(f);
             }
         }
     }
 
-    public static String getFileNameNoEx(String filename) { 
+    public String getFileNameNoEx(String filename) { 
         if ((filename != null) && (filename.length() > 0)) { 
             int dot = filename.lastIndexOf('.'); 
             if ((dot >-1) && (dot < (filename.length()))) { 
@@ -95,23 +128,27 @@ public class Main {
     } 
 }
 
-class FileInfo {
-    private String fileName;
-    private String filePath;
-
-    FileInfo(File file){
-        fileName = file.getName();
-        filePath = file.getPath();
-    }
-    FileInfo(String n, String p){
-        fileName = n;
-        filePath = p;
-    }
-    public String getFileName(){
-        return fileName;
-    }
-
-    public String getFilePath(){
-        return filePath;
+class FileWorker {
+    public void writeFoundFileInfo(String[] searchingDirs, String[] searchQueries, String writeToPath){
+        try{
+            FileExplorer filex = new FileExplorer();
+            ArrayList<File> extractedFiles = filex.getAllFiles(searchingDirs);//produce all files given the array of directories that need to be searched
+            ArrayList<FileInfo> informations = filex.getMatchedFileInfo(extractedFiles, searchQueries);//produce all files that contain given string in array called searchQueries
+            File file = new File(writeToPath);
+            if(!file.exists()){
+                file.createNewFile();
+            }
+            FileWriter fileWriter = new FileWriter(file.getName(),true);
+            BufferedWriter myWriter = new BufferedWriter(fileWriter);
+            // myWriter.write("Total " + "'" + search + "'" + "found : " + informations.size());
+            for(FileInfo info : informations){
+                myWriter.write(info.concreteInfo());
+            }
+            myWriter.close();
+            System.out.println("done");
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
     }
 }
